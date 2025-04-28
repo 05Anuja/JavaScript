a147c3ad294a0af84fe2c2c9862aadd49f0b0a17
AP1905 <anuja13122004@gmail.com>
2025-04-28 23:54:13 +0530

    factorial

--- JS/map.js
+++ JS/map.js
@@ -1,10 +1,19 @@
-let arr = [1,2,3,4,5];
+let arr = [1, 2, 3, 4, 5];
 
 let newArr = arr.map(function (n) {
-    let res = 1;
-    for (let i=1;i<=n;i++) {
-        res *= i;
-    }
-    return res;
+  let res = 1;
+  for (let i = 1; i <= n; i++) {
+    res *= i;
+  }
+  return res;
 });
 console.log(newArr);
+
+function fact_num(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  return num * fact_num(num - 1);
+}
+// console.log(fact_num(6));
+console.log(fact_num(5));
