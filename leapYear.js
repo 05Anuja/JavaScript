87a9e193fcc5f34932442c078d20f29fbf1c5242
AP1905 <anuja13122004@gmail.com>
2025-05-08 19:02:22 +0530

    Leap Year using Function

--- leapYear.js
+++ leapYear.js
@@ -1,4 +1,4 @@
-let year = Number(prompt("Enter the Year"));
+// let year = Number(prompt("Enter the Year"));
 
 // if ((year %4===0 && year %100 !==0) || year %400 ===0) {
 //     document.writeln(`${year} is a Leap Year`);
@@ -6,9 +6,29 @@ let year = Number(prompt("Enter the Year"));
 //     document.writeln(`${year} is not a Leap Year`);
 // }
 
-if (year % 4 === 0 && year % 100 !== 0) {
-  document.writeln(`${year} is a Leap Year`);
-} else if (year % 400 === 0) {
+// if (year % 4 === 0 && year % 100 !== 0) {
+//   document.writeln(`${year} is a Leap Year`);
+// } else if (year % 400 === 0) {
+//   document.writeln(`${year} is a Leap Year`);
+// } else {
+//   document.writeln(`${year} is not a Leap Year`);
+// }
+
+function leapYear(year) {
+  if (year % 4 === 0 && year % 100 !== 0) {
+    // document.writeln(`${year} is a Leap Year`);
+    return true;
+  } else if (year % 400 === 0) {
+    // document.writeln(`${year} is a Leap Year`);
+    return true;
+  } else {
+    // document.writeln(`${year} is not a Leap Year`);
+    return false;
+  }
+}
+
+let year = Number(prompt("Enter the year"));
+if (leapYear(year) === true) {
   document.writeln(`${year} is a Leap Year`);
 } else {
   document.writeln(`${year} is not a Leap Year`);
