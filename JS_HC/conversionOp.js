61cd8547494e232501e0ed5441e18338410ee31a
Anuja-Bhimraj-Pawar <anuja13122004@gmail.com>
2025-05-13 23:32:16 +0530

    Conversion Operations

---
+++ JS_HC/conversionOp.js
@@ -0,0 +1,44 @@
+let score = "33";
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+let isLoggedIn = "Anuja";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false; "" => false; "Anuja" => true
+
+let randomNum = 33;
+let stringNum = String(randomNum);
+// console.log(typeof stringNum);
+
+// ***********Operations*****************
+let val = 13;
+let negVal = -val; // -13
+// console.log(negVal);
+
+// concatenation
+let str1 = "Anuja ";
+let str2 = "Pawar";
+// console.log(str1 + str2);
+
+// console.log(+true);
+// console.log(+false);
+// console.log(+"");
+
+// Not a correct way to write the code
+let num1, num2, num3;
+num1 = num2 = num3 = 4;
+// console.log(num1, num2, num3);
+
+let val1 = 10;
+// val1++;
+// console.log(val1++);
+// in the above first it will print then increment the value
+
+let val2 = 20;
+// ++val2;
+// console.log(++val2);
+// in the above the value will be incremented first then it will print
