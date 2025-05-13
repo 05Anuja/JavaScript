bc0714d1d518dda15e693013a564d4da81f03084
Anuja-Bhimraj-Pawar <anuja13122004@gmail.com>
2025-05-13 15:16:21 +0530

    Datatypes

---
+++ JS_HC/datatypes.js
@@ -0,0 +1,10 @@
+// "use strict"; // treat all JS code as newer version/JS
+let name = "Anuja" // string
+let age = 21; // number (range - 2 to 2^53)
+let isLoggedIn = false ; //boolean
+
+/*
+There are two types of datatypes in JS - Primitive and Non-Primitive
+1) Primitive Datatypes = number (1,2,3), string ("hello", "anuja"), boolean (true, false), null (standalone value), undefined (declared but not assigned), symbol (creates a unique value)
+2) Non-Primitive Datatypes = object (array([1,2,3]), function (function() {}))
+*/
