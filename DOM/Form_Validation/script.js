ae824225608f46fef796441ec35f852981e69b0a
Anuja-Bhimraj-Pawar <anuja13122004@gmail.com>
2025-05-21 18:55:38 +0530

    Sign-In Form Validations

--- DOM/Form_Validation/script.js
+++ DOM/Form_Validation/script.js
@@ -24,3 +24,44 @@ function closeBtnFunc() {
 //     overlay.classList.remove('active');
 //   }
 // });
+
+
+
+function formSubmit(event) {
+    event.preventDefault();
+    let userName = document.getElementById("username").value;
+    const usernameRegex  = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*_)[a-zA-Z0-9_]{3,16}$/;
+    if (userName === "") {
+        document.getElementsByClassName("error")[0].innerHTML = "This field is required";
+    } else if (!usernameRegex.test(userName)) {
+        document.getElementsByClassName("error")[0].innerHTML = "Username must be 3–16 characters and must contain letters, numbers, and underscores only.";
+    } else {
+        document.getElementsByClassName("error")[0].innerHTML = "";
+        console.log(userName);
+    }
+
+    let password = document.getElementById("password").value;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (password === "") {
+        document.getElementsByClassName("error")[1].innerHTML = "This field is required";
+    } else if (!passwordRegex.test(password)) {
+        document.getElementsByClassName("error")[1].innerHTML = "Password must be at least 8 characters long and must include uppercase, lowercase, number, and special character.";
+    } else {
+        document.getElementsByClassName("error")[1].innerHTML = "";
+        console.log(password);
+    }
+
+    let confirmPassword = document.getElementById("confirmPassword").value;
+    if (confirmPassword === "") {
+        document.getElementsByClassName("error")[2].innerHTML = "This field is required";
+    } else if (confirmPassword !== password) {
+        document.getElementsByClassName("error")[2].innerHTML = "Password does not match";
+    } else {
+        document.getElementsByClassName("error")[2].innerHTML = "";
+        console.log(confirmPassword);
+    }
+
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("confirmPassword").value = "";
+}
