ff3b81e147cc93e6168de8dfcc8fe7e5d9794ca3
Anuja-Bhimraj-Pawar <anuja13122004@gmail.com>
2025-05-28 10:02:05 +0530

    If Else

---
+++ JS_HC/Control_Flow/one.js
@@ -0,0 +1,40 @@
+// 1) If
+
+// if (true) {
+//     console.log("Hii");
+// }
+
+// 2) If....else
+// if (2 === "2") {
+//     console.log(`2 is equal to "2"`);
+// } else {
+//     console.log(`2 is not equal to "2"`);
+// }
+
+// let num = 5;
+// if (num % 2 === 0) {
+//     console.log(`${num} is Even Number`);
+// } else {
+//     console.log(`${num} is Odd Number`);
+// }
+
+// 3) Nested if...else
+// let num = 50;
+// if (num > 0) {
+//     console.log(`${num} is Positive Number`);
+// } else if (num < 0) {
+//     console.log(`${num} is Negative Number`);
+// } else {
+//     console.log(`${num} is Zero`);
+// }
+
+// 4) && and ||
+// For && - If both conditions are true then only the code will be executed
+// if (2 === 2 && 3 !== 3) {
+//   console.log(`Executed`);
+// } 
+
+// For || - If one of the condition is true then code will be executed
+if (2=== 2 || 2 === "2") {
+    console.log(`Executed`);
+}
