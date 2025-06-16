0322760fb67446ac5b021e2d20df464f1d6d0cfd
Anuja-Bhimraj-Pawar <anuja13122004@gmail.com>
2025-06-16 20:12:46 +0530

    while and doWhile loops

---
+++ JS_HC/Control_Flow/while_doWhile.js
@@ -0,0 +1,18 @@
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// let arr = ["Anuja", "Aryan", "Poonam", "Bhimraj"];
+// let i = 0;
+// while (i < arr.length) {
+//     console.log(`Array Value at ${i}: ${arr[i]}`);
+//     i++;
+// }
+
+let score = 2;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score < 1);
