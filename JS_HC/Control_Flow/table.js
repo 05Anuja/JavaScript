0aa5480462fb432ca1ff88f0de1e5cc2006b37c2
Anuja-Bhimraj-Pawar <anuja13122004@gmail.com>
2025-06-16 13:05:16 +0530

    break and continue

--- JS_HC/Control_Flow/table.js
+++ JS_HC/Control_Flow/table.js
@@ -1,6 +1,16 @@
 for (let i = 1; i <= 10; i++) {
     for (let j = 1; j<= 10; j ++) {
-        console.log(`${i} X ${j} = ${i*j}`)
+        // console.log(`${i} X ${j} = ${i*j}`)
     }
-    console.log("\n")
+    // console.log("\n")
+}
+
+
+// break and continue
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        console.log(`5 Detected`);
+        break;
+    }
+    console.log(i)
 }
