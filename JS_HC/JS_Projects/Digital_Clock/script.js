1e555494bc17ee0e68843fa736133f1538c4ae4f
Anuja-Bhimraj-Pawar <anuja13122004@gmail.com>
2025-06-23 22:25:37 +0530

    JS

---
+++ JS_HC/JS_Projects/Digital_Clock/script.js
@@ -0,0 +1,14 @@
+const clock = document.querySelector("#clock");
+
+setInterval (() => {
+    const date = new Date();
+    let hrs = date.getHours();
+    let mins = date.getMinutes();
+    let secs = date.getSeconds();
+    
+    hrs = hrs < 10 ? `0${hrs}` : hrs;
+    mins = mins < 10 ? `0${mins}` : mins;
+    secs = secs < 10 ? `0${secs}` : secs;
+
+    clock.innerHTML = `${hrs} : ${mins} : ${secs}`;
+}, 1000)
